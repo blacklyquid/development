f2653822e4fec9aeca078354eea92105be37d8ff
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-14 05:50:12 -0400

    Update stream_capture.py

--- stream_capture.py
+++ stream_capture.py
@@ -6,7 +6,8 @@ class stream_capture:
 	def __init__(self, stream_url):
 		self.url = stream_url
 		self.stream = cv2.VideoCapture(self.url)
-	
+		if not self.stream.isOpened():
+			
 	def get_blob(self):
 		while True:
 			ret, frame = self.stream.read()
