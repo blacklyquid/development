925ec660679f7ab7cd0c2c5fbef8f6cca8b369ec
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-14 16:00:51 -0400

    Add files via upload

---
+++ lyquid/stream_capture.py
@@ -0,0 +1,22 @@
+# stream_capture.py
+
+import cv2, imutils
+
+class stream_capture:
+	
+	def __init__(self, stream_url):
+		self.url = stream_url
+		self.stream = cv2.VideoCapture(self.url)
+		if not self.stream.isOpened():
+			sys.exit('Having trouble opening video stream.' + self.url)
+
+	def get_blob(self):
+		while True:
+			ret, frame = self.stream.read()
+			if ret:
+				frame = imutils.resize(frame, width=400)
+				blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+				return blob
+	
+	def __del__(self):
+		self.stream.release()
