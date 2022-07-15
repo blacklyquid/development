3bd9dcfc12a4f74f1413d6dcada91e460f330cb0
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-14 21:25:42 -0400

    Update object_detector.py

--- lyquid/object_detector.py
+++ lyquid/object_detector.py
@@ -3,19 +3,6 @@
 import cv2, time
 import numpy as np
 
-class detected_object:
-	labels = ["background", "aeroplane", "bicycle", "bird", 
-	"boat","bottle", "bus", "car", "cat", "chair", "cow", 
-	"diningtable","dog", "horse", "motorbike", "person", "pottedplant", 
-	"sheep","sofa", "train", "tvmonitor"]
-	def __init__(self, detected_confidence, label_index):
-		self.confidence = detected_confidence
-		self.label = self.labels[label_index]
-		self.timestamp = time.time()
-		self.label_index = label_index
-		self.json_string = '{ "object":"' + self.label + '", "idx":"' + str(self.label_index) + '","confidence":"' + str(self.confidence) + '","time":"' + str(self.timestamp) + '"}'
-	def __str__(self):
-		return self.json_string
 
 class object_detector:
 	def __init__( self, min_confidence, prototxt, model ):
@@ -28,6 +15,6 @@ class object_detector:
 		dol = []
 		for i in np.arange(0, nn_detections.shape[2]):
 			if nn_detections[0, 0, i, 2] > self.min_confidence:
-				dol.append(detected_object( nn_detections[0, 0, i, 2], int(nn_detections[0, 0, i, 1])))
+				dol.append(detected_object( nn_detections[0, 0, i, 2], int(nn_detections[0, 0, i, 1], detections[0, 0, i, 3:7])))
 
 		return dol
