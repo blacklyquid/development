dd469d16801dd5c18d1b345ae035d9d5bc372681
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-14 21:32:22 -0400

    Update stream_capture.py

--- lyquid/stream_capture.py
+++ lyquid/stream_capture.py
@@ -7,15 +7,16 @@ class stream_capture:
 	def __init__(self, stream_url):
 		self.url = stream_url
 		self.stream = cv2.VideoCapture(self.url)
+		self.frame = None
 		if not self.stream.isOpened():
 			sys.exit('Having trouble opening video stream.' + self.url)
 
 	def get_blob(self):
 		while True:
-			ret, frame = self.stream.read()
+			ret, self.frame = self.stream.read()
 			if ret:
-				frame = imutils.resize(frame, width=400)
-				blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+				self.frame = imutils.resize(frame, width=400)
+				blob = cv2.dnn.blobFromImage(cv2.resize(self.frame, (300, 300)), 0.007843, (300, 300), 127.5)
 				return blob
 	
 	def __del__(self):
