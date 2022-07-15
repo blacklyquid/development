a263a1c6ada36cd9857d2857606168374593e927
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-15 06:07:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,29 +17,25 @@ vs = stream_capture(Config.STREAM_URL)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(vs),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-  
-if __name__ == "__main__":
-	
-	app.run(host='0.0.0.0', debug=True)
-	
-	# sleeping might reset connection on camera
-	print("Waiting 60 seconds to start ...",flush=True)
-	time.sleep(60.0)
-	print("Begining...",flush=True)
-
 	while True:
 		detections = detector.get_detections( vs.get_blob() )
 		for detection in detections:
 			mqtt.publish( Config.MQTT_TOPIC + "/" + detection.label, str(detection) )
+		frame = camera.get_frame()
+		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    	return Response(gen(vs),mimetype='multipart/x-mixed-replace; boundary=frame')
+  
+if __name__ == "__main__":
+	# sleeping might reset connection on camera
+	print("Waiting 60 seconds to start ...",flush=True)
+	time.sleep(60.0)
+	print("Begining...",flush=True)
+	
+	app.run(host='0.0.0.0', debug=True)
+		
