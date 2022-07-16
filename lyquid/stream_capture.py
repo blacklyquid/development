d002bc51c34ea6e2b9464a63bca997e6a8145e09
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-15 21:53:26 -0400

    Update stream_capture.py

--- lyquid/stream_capture.py
+++ lyquid/stream_capture.py
@@ -3,7 +3,7 @@
 import cv2, imutils
 
 class stream_capture:
-	
+
 	def __init__(self, stream_url):
 		self.url = stream_url
 		self.stream = cv2.VideoCapture(self.url)
@@ -13,22 +13,22 @@ class stream_capture:
 			sys.exit('Having trouble opening video stream.' + self.url)
 
 	def get_blob(self):
-            if not self.new_frame:
-                self.read()
-            self.frame = imutils.resize(self.frame, width=400)
-            blob = cv2.dnn.blobFromImage(cv2.resize(self.frame, (300, 300)), 0.007843, (300, 300), 127.5)
-            self.new_frame = False
-            return blob
-	
+		if not self.new_frame:
+			self.read()
+		self.frame = imutils.resize(self.frame, width=400)
+		blob = cv2.dnn.blobFromImage(cv2.resize(self.frame, (300, 300)), 0.007843, (300, 300), 127.5)
+		self.new_frame = False
+		return blob
+
 	def read(self):
-        	self.new_frame = True
+		self.new_frame = True
 		ret, self.frame = self.stream.read()
 
-    def to_jpeg_bytes(self):
-        if not self.frame:
-            self.read()
-        ret, jpeg = cv2.imencode('.jpg', self.frame)
-        return jpeg.tobytes()
-    
+	def to_jpeg_bytes(self):
+		if not self.frame:
+			self.read()
+		ret, jpeg = cv2.imencode('.jpg', self.frame)
+		return jpeg.tobytes()
+
 	def __del__(self):
 		self.stream.release()
