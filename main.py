c8309a074d957f76fa93fdb7577cb7ae46b7f30a
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-17 17:18:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 # main.py
-
+import numpy as np
+import cv2
 import time
 
 from flask import Flask, render_template, Response
 
-from lyquid.config import Config
-from lyquid.detected_object import detected_object
-from lyquid.object_detector import object_detector
-from lyquid.paho_mqtt import paho_mqtt
-from lyquid.stream_capture import stream_capture
-
 app = Flask(__name__)
-mqtt = paho_mqtt( Config.MQTT_CLIENT_ID, Config.MQTT_USER, Config.MQTT_PASSWORD, Config.MQTT_HOST, Config.MQTT_PORT, Config.THROTTLE_TIME)
-detector = object_detector(Config.MIN_CONFIDENCE, Config.FILE_PROTOTXT, Config.FILE_MODEL)
-vs = stream_capture(Config.STREAM_URL)
 
 @app.route('/')
 def index():
@@ -36,10 +28,41 @@ def video_feed():
 	return Response(gen(vs),mimetype='multipart/x-mixed-replace; boundary=frame')
   
 if __name__ == "__main__":
-    # sleeping might reset connection on camera
-    print("Waiting 60 seconds to start ...",flush=True)
-    #time.sleep(60.0)
-    print("Begining...",flush=True)
 
     app.run(host='0.0.0.0', debug=True)
 		
+
+
+PROTOTXT = "MobileNetSSD_deploy.prototxt"
+MODEL = "MobileNetSSD_deploy.caffemodel"
+INP_VIDEO_PATH = 'cars.mp4'
+OUT_VIDEO_PATH = 'cars_detection.mp4'
+GPU_SUPPORT = 0
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",  "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+if GPU_SUPPORT:
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+    
+cap = cv2.VideoCapture(INP_VIDEO_PATH)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+       break
+    h, w = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+    net.setInput(blob)
+    detections = net.forward()
+    for i in np.arange(0, detections.shape[2]):
+       confidence = detections[0, 0, i, 2]
+       if confidence > 0.5:
+           idx = int(detections[0, 0, i, 1])
+           box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+           (startX, startY, endX, endY) = box.astype("int")
+           label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
+           cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
+           y = startY - 15 if startY - 15 > 15 else startY + 15
+           cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+ 
