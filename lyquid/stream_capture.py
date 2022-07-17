0cfce10430076be3ebd041ab235f19556df7fe5d
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-17 07:23:31 -0400

    Update stream_capture.py

--- lyquid/stream_capture.py
+++ lyquid/stream_capture.py
@@ -1,6 +1,7 @@
 # stream_capture.py
 
 import cv2, imutils
+import numpy as np
 
 class stream_capture:
 
@@ -28,7 +29,12 @@ class stream_capture:
 	def to_jpeg_bytes(self):
 		ret, jpeg = cv2.imencode('.jpg', self.frame)
 		return jpeg.tobytes()
-	def frame_add_box(self, box):
-		
+	def frame_add_box(self, object):
+		box = object.box * np.array([self.frame_w, self.frame_h, self.frame_w, self.frame_h])
+           	(startX, startY, endX, endY) = box.astype("int")
+           	label = "{}: {:.2f}%".format(object.label,confidence*100)
+           	cv2.rectangle(self.frame, (startX, startY), (endX, endY), object.color, 2)
+           	y = startY - 15 if startY - 15 > 15 else startY + 15
+           	cv2.putText(self.frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 	def __del__(self):
 		self.stream.release()
