d433b43b2f7d06f26882c3f55f320c2e5b4d4714
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-17 17:55:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -37,13 +37,13 @@ def gen(camera):
 		for i in np.arange(0, detections.shape[2]):
 			confidence = detections[0, 0, i, 2]
 			if confidence > 0.5:
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-			label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
-			y = startY - 15 if startY - 15 > 15 else startY + 15
-			cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+				label = "{}: {:.2f}%".format(CLASSES[idx],confidence*100)
+				cv2.rectangle(frame, (startX, startY), (endX, endY),    COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 		frame = cv2.imencode('.jpg', frame)
 		yield (b'--frame\r\n'
 			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
